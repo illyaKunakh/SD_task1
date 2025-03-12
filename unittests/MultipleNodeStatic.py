013be5619e01b378479328d11966901428461446
Illya Kunakh <illya.kunakh@estudiants.urv.cat>
2025-03-12 13:39:47 +0100

    unittest defined

---
+++ unittests/MultipleNodeStatic.py
@@ -0,0 +1,3 @@
+"""Multiple-nodes static scaling performance analysis: Stress test both services using now
+one, two, and three nodes and calculate speedups obtained in the four systems.
+Modify the architectures if needed to accommodate more servers."""
---
+++ unittests/SingleNode.py
@@ -0,0 +1,4 @@
+"""Single-node performance analysis: Stress test both services using a single node
+(InsultService, InsultFilter) to understand how many requests can process. Compare
+the four systems using plots with the results of the four implementations and reason
+about those results"""
